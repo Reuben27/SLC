4b5f388cbf83a98bc063dbf75cc94f739c62c28b
Reuben27 <reuben.sd@iitgn.ac.in>
2021-01-03 13:25:23 +0530

    Updates

--- public/js/slc2021.js
+++ public/js/slc2021.js
@@ -1,6 +1,16 @@
-window.onscroll = function() {myFunction()};
+var backtotop = document.getElementById("myBtn");
 
-function myFunction() {
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    backtotop.style.display = "block";
+  } 
+  else {
+    backtotop.style.display = "none";
+  }
+
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
