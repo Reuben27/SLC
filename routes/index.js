8176bd55c24b37b7ee6f48db21bf2154c4836207
Reuben27 <reuben.sd@iitgn.ac.in>
2021-08-17 19:55:39 +0530

    slc 2021 sod

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,38 @@ var router = express.Router()
 
 router.get("/", function (req, res) {
     res.render("Home")
-})
+});
 
 router.get("/slc-2021", function (req, res) {
     res.render("SLC-2021")
-})
+});
 
 router.get("/prev-editions", function (req, res) {
     res.render("Previous-Editions")
-})
+});
 
 router.get("/gallery", function (req, res) {
     res.render("Gallery")
-})
+});
 
 router.get("/team", function (req, res) {
     res.render("Team")
-})
+});
 
-router.get("/slc-2018", function (req, res) {
+router.get("/sod/slc-2018", function (req, res) {
     res.render("SODs/SLC-2018")
-})
+});
 
-router.get("/slc-2019", function (req, res) {
+router.get("/sod/slc-2019", function (req, res) {
     res.render("SODs/SLC-2019")
-})
+});
 
-router.get("/slc-2020", function (req, res) {
+router.get("/sod/slc-2020", function (req, res) {
     res.render("SODs/SLC-2020")
+});
+
+router.get("/sod/slc-2021", function (req, res) {
+    res.render("SODs/SLC-2021")
 })
 
 module.exports = router
